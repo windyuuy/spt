4d1a4f720032f3021f82d0c68d7f888f5ababfe7
kugoo11v12@126.com <kugoo11v12@126.com>
2015-09-01 03:33:35 -0400

    aefwwer

--- src/lua_ext.lua
+++ src/lua_ext.lua
@@ -5,6 +5,13 @@ function requirelist(list)
 	end
 end
 
+-- sudo mount -t ntfs-3g /dev/sda5 /home/happy/devs/proc -ro force
+
+--require('socket')
+
+package.path=package.path..';/home/happy/devs/proc/PCOM/lua/5.1/lua/?.lua;'
+requirelist({'socket','strbuf','list','string_ext','dump'})
+
 requirelist({'strbuf','list','string_ext','dump'})
 
 function clear_mem(count)
