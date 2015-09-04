73de4388cd6e4bbdc5401c6c47b841b56538eb17
windyuuy <kugoo11v12@126.com>
2015-09-04 19:25:40 +0900

    fill little content of README.md

--- src/test/test_op_file.lua
+++ src/test/test_op_file.lua
@@ -1,37 +1,25 @@
 
 require('lua_ext')
-
-require('checker')
-
-require('sparser')
-
-require('lfs')
+requirelist({
+	'checker','sparser',
+	'lfs'
+	})
 
 local function main()
 	sheet('kkk')
 	lfs.chdir('./src/test')
+
 	local f=io.open('rule.rd')
---	for line in f:lines() do
---		ssline=runner.runline(line)
-----##dd=$line{[/print/] bracket word['n'] bracket}
-----		g=_G
-----		_G=_M
---		func=assert(loadstring(ssline),ssline)
---		setfenv(func,getfenv(1))
---		func()
---	end
---	
 	runner.runcontent(f:lines())
+
 	local hf=io.open('hello.c')
-	local sfd=lineinfo:create(hf:lines()())
---	print(sfd)
-	result=dd:check(sfd)
---	rdump(result)
---	result=dd:check(lineinfo:create('print(hello)'))
---	rdump(result)
+	local info=lineinfo:create(hf:lines()())
+	
+	result=dd:check(info)
 
 	print(result:index('krk>@rawline'))
 
+	hf:close()
 	f:close()
 end
 
